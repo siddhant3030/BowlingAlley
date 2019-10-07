5c8e8d210e1027c2a943962f191dcff166715e17
siddhant3030 <ssiddhant3030@gmail.com>
2019-10-07 22:40:21 +0530

    new changes

--- ruby/game.rb
+++ ruby/game.rb
@@ -1,61 +1,58 @@
-require 'enum'
 class Game
-    attr_accessor :lane, :players, :num_sets, chance_per_sets
-    @counter = 0
-    @num_sets = 10
-    @chance_per_set =  2
+  attr_accessor :lane, :players, :num_sets, :chance_per_sets
 
-    def initialize(lane, players)
-      @lane = lane
-      @players = players
-    end
+  def initialize(lane, players, num_sets = 10, chance_per_set = 2)
+    @lane = lane
+    @players = players
+    @num_sets = num_sets
+    @chance_per_set = chance_per_set
+  end
 
-    def take_shot(standing_pins)
-        return 0 + rand(0..standing_pins)
-    end
+  def take_shot(standing_pins)
+    rand(0..standing_pins)
+  end
 
-    def start
-        while @counter < @num_sets do
-          for i in 0..@players.length do
-            player = @players[i]
+  def start
+    @num_sets.times do |set_number|
+     p "Set #{set_number + 1}"
+      @players.each_with_index do |player, i|
+        standing_pins = 10
+        total_pin_down = 0
+        @chance_per_set.times do |n|
+          number_pin_down = take_shot(standing_pins)
+          total_pin_down += number_pin_down
+          standing_pins -= number_pin_down
+          if n.zero? && number_pin_down == 10
+            p "There's a strike by player #{player.name}"
+            player.add_score(20)
             standing_pins = 10
-            total_pin_down = 0
-            p Enum.new.generate_score
-            for n in 0..@chance_per_set do
-                number_pin_down = take_shot(standing_pins)
-                total_pin_down += number_pin_down
-                standing_pins -= number_pin_down
-
-                if n == 0 and number_pin_down == 10
-                  puts "There's a strike by player #{player.name}"
-                  player.add_score(20)
-                  standing_pins = 10 
-                elsif n == 1 and standing_pins == 0
-                  puts "There's a spare by player #{player.name}"
-                  player.add_score(number_pin_down + 5)
-                else
-                  spare"#{player.name} pinned down #{number_pin_down} in #{(n+1)} shot"
-                  player.add_score(number_pin_down)
-                end
-            end
-          @counter += 1
-        end
-      end
-    end
-
-
-
-    def winner
-      max_score = -1, score
-      name = nil
-      for p in 0..@players.length
-          player = @players[p]
-          score = player.score
-          if score > max_score
-              max_rscore = player.score
-              name = player.name
+          elsif n == 1 && standing_pins.zero?
+            p "There's a spare by player #{player.name}"
+            player.add_score(number_pin_down + 5)
+          elsif set_number == @num_sets && standing_pin_down.zero?
+            p 'Player gets an exta chance'
+            n -= 1
+          else
+            p "#{player.name} pinned down #{number_pin_down} in shot #{n + 1}"
+            player.add_score(number_pin_down)
           end
+        end
+        p "Current score for #{player.name} = #{player.get_score}"
       end
-      return name
-    end     
+end
+  end
+
+  def winner
+    max_score = -1, score
+    name = nil
+    for p in 0..@players.length
+        player = @players[p]
+        score = player.score
+        if score > max_score
+            max_rscore = player.score
+            name = player.name
+        end
+    end
+    name
+  end    
 end
--- ruby/player.rb
+++ ruby/player.rb
@@ -1,22 +1,18 @@
 class Player
     attr_accessor :id, :name, :total_score
-    @total_score = 0
     def initialize(id, name)
         @id = id
         @name = name
+        @total_score = 0
     end
     
     # we user attr_accessor to create getters and setters.
     def add_score(score)
         @total_score += score
-    end    
-end
-
-# sid = Player.new(1, 'Sid')
-# puts sid.name
-
-
+    end
     
-
-
-
+    def get_score
+     @total_score
+    end
+ end
+ 
